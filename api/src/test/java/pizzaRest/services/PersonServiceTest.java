bd43aac66dcba147fa2e76a81884aa31850adafb
Bugaienko <sergiibugaienko@gmail.com>
2023-04-24 21:39:35 +0200

    Update PersonServiceTest.java

--- api/src/test/java/pizzaRest/services/PersonServiceTest.java
+++ api/src/test/java/pizzaRest/services/PersonServiceTest.java
@@ -1,13 +1,17 @@
 package pizzaRest.services;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pizzaRest.repositiries.PersonRepository;
 
+//@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 //@ImportAutoConfiguration
-//@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = PersonServiceTest.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = PersonServiceTest.class)
 class PersonServiceTest {
 
     private final PersonRepository personRepository;
